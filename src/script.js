bcc2f08f6154288acd7d666c37b6bf4a747d3c0d
nastaj <G00424689@atu.ie>
2023-05-02 10:48:54 +0100

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const inputTaskEl = document.querySelector(".inputTask");
+const addTaskBtn = document.querySelector(".addTaskBtn");
+const remainingEl = document.querySelector(".remaining");
+const completedEl = document.querySelector(".completed");
+const totalEl = document.querySelector(".total");
+const taskListEl = document.querySelector(".taskList");
